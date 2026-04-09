94578cfb14cbe11904233551205093a3adc20e73
Tilak Raj <rjtilak36@gmail.com>
2026-04-09 23:52:20 +0530

    Hierarchical Inheritance

---
+++ Basics/238_OOPS/252_hierarchical_inheritance.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+
+class Animal{
+public:
+    void eat(){
+        cout<<"eats\n";
+    }
+
+    void breathe()
+    {
+        cout<< "breathes\n";
+    }
+};
+
+class Bird : public Animal{
+public:
+    void fly()
+    {
+        cout<<"Flies\n";
+    }
+
+};
+
+class Fish : public Animal{
+public:
+    void swim()
+    {
+        cout<<"Swims\n";
+    }
+
+};
+class mammal : public Animal{
+public:
+    void walk()
+    {
+        cout<<"walks\n";
+    }
+
+};
+
+
+int main()
+{
+
+
+    mammal m1;
+    m1.eat();
+    m1.breathe();
+
+    m1.walk();
+    
+
+
+    return 0;
+}
