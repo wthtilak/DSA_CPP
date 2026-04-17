aecbf0a4f813e6142144ae7a7d603d63113e6078
Tilak <rjtilak36@gmail.com>
2026-04-17 12:24:00 +0530

    Last Occurrence.

---
+++ 17.Recursion/09_last_Ocurrence.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+int lastOccur(vector<int> arr, int i, int target) {
+  if (i == arr.size())
+    return -1;
+
+  int idxFound = lastOccur(arr, i + 1, target);
+  if (idxFound == -1 && arr[i] == target) {
+    return i;
+  }
+
+  return idxFound;
+}
+
+int main() {
+
+  vector<int> arr = {1, 2, 3, 3, 3, 4};
+
+  cout << "Target at index :" << lastOccur(arr, 0, 3);
+}
