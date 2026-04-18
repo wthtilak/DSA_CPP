2f84d84e8ac72f544f534e68480e2c9633335e19
Tilak <rjtilak36@gmail.com>
2026-04-18 17:41:48 +0530

    Tiling Problem

--- 17.Recursion_2/01_tiling_problem.cpp
+++ 17.Recursion_2/01_tiling_problem.cpp
@@ -2,9 +2,23 @@
 #define endl '\n'
 using namespace std;
 
+// Tiling Problem (2 x n)floors with (2 x 1)Tiles
+
+int tiling_problem(int n) {
+  if (n == 0 || n == 1)
+    return 1;
+  // vertical
+  int ans1 = tiling_problem(n - 1);
+
+  // horizontal
+  int ans2 = tiling_problem(n - 2);
+
+  return ans1 + ans2;
+}
 
 int main() {
 
+  cout << tiling_problem(4);
 
-    return 0;
+  return 0;
 }
