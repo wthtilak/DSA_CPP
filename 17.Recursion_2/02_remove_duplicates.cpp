f483512864c3a924697584cd3f43ba8c2e748713
Tilak <rjtilak36@gmail.com>
2026-04-20 15:45:45 +0530

    Remove Duplicates without index

--- 17.Recursion_2/02_remove_duplicates.cpp
+++ 17.Recursion_2/02_remove_duplicates.cpp
@@ -3,13 +3,14 @@
 #define endl '\n'
 using namespace std;
 
-//with using index i
+// with using index i
 void removeDp(string str, string ans, int i, int map[26]) {
 
   if (i == str.size()) {
     cout << ans << " " << endl;
     return;
   }
+
   int mapIdx = (int)(str[i] - 'a');
   if (map[mapIdx]) {
     removeDp(str, ans, i + 1, map);
@@ -19,10 +20,35 @@ void removeDp(string str, string ans, int i, int map[26]) {
   }
 }
 
+// without using index i
+void removeDuplicates(string str, string ans, int map[26]) {
+
+  if (str.size() == 0) {
+    cout << ans << " " << endl;
+    return;
+  }
+
+  int n = str.size();
+  char ch = str[n - 1];
+  int mapIdx = (int)(str[n - 1] - 'a');
+  str = str.substr(0, n - 1);
+
+  if (map[mapIdx]) // duplicate string
+  {
+    removeDuplicates(str, ans, map);
+  } else { // not duplicate
+    map[mapIdx] = true;
+    removeDuplicates(str, ch + ans, map);
+  }
+}
+
 int main() {
   string str = "appnnacollege";
   string ans = "";
   int map[26] = {false};
-  removeDp(str, ans, 0, map);
+  
+  removeDp(str, ans, 0, map); // apncoleg
+
+  //   removeDuplicates(str, ans, map); // pnacolge
   return 0;
 }
