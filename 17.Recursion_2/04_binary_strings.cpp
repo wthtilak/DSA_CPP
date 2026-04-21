1f4cfc910a2bbde39ebf9953396dcbdf33a61731
Tilak <rjtilak36@gmail.com>
2026-04-21 16:39:07 +0530

    Added function without lastPlace variable.

--- 17.Recursion_2/04_binary_strings.cpp
+++ 17.Recursion_2/04_binary_strings.cpp
@@ -3,6 +3,7 @@
 #define endl '\n'
 using namespace std;
 
+// with lastPlace variable
 void binStrings(int n, int lastPlace, string ans) {
   if (n == 0) {
     cout << ans << endl;
@@ -16,9 +17,25 @@ void binStrings(int n, int lastPlace, string ans) {
   }
 }
 
+// without lastPlace Variable
+void binStrings(int n, string ans) {
+  if (n == 0) {
+    cout << ans << endl;
+    return;
+  }
+  if (ans.size() == 0 || ans[ans.size() - 1] != '1') {
+    binStrings(n - 1, ans + '0');
+    binStrings(n - 1, ans + '1');
+  } else {
+    binStrings(n - 1, ans + '0');
+  }
+}
+
 int main() {
 
   string ans = "";
-  binStrings(3, 0, ans);
+  // binStrings(3, 0, ans);
+  
+  binStrings(3, ans);
   return 0;
 }
