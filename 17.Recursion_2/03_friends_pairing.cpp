b5b25c72bd52c7b16ffd6c4e5f6107ad1f2d5218
Tilak <rjtilak36@gmail.com>
2026-04-21 14:45:20 +0530

    Fixed code & added Comments

--- 17.Recursion_2/03_friends_pairing.cpp
+++ 17.Recursion_2/03_friends_pairing.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int friendsPairing(int n) {
-  if (n == 0 || n == 1)
+  if (n == 1 || n == 2)
     return n;
 
   return friendsPairing(n - 1) + (n - 1) * friendsPairing(n - 2);
@@ -11,7 +11,8 @@ int friendsPairing(int n) {
 
 int main() {
 
-  cout << friendsPairing(3);
+  cout << friendsPairing(3)<<endl; // ways 4
+  cout << friendsPairing(4); // ways 10
 
   return 0;
 }
