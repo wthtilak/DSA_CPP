8e4d437171b1e2d6c6f099bc454d1e5af5d7ba89
Tilak <rjtilak36@gmail.com>
2026-04-22 16:49:21 +0530

    Merge Sort

---
+++ 18. Divide & Conquer/01.mergeSort.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+void merge(int arr[], int si, int mid, int ei) {
+  vector<int> temp;
+  int i = si;
+  int j = mid + 1;
+
+  while (i <= mid && j <= ei) {
+    if (arr[i] < arr[j]) {
+      temp.push_back(arr[i++]);
+    } else {
+      temp.push_back(arr[j++]);
+    }
+  }
+
+  while (i <= mid) {
+    temp.push_back(arr[i++]);
+  }
+
+  while (j <= ei) {
+    temp.push_back(arr[j++]);
+  }
+
+  // vector to original printArray
+  int x = 0;
+  for (int idx = si; idx <= ei; idx++) {
+    arr[idx] = temp[x++];
+  }
+}
+
+void mergeSort(int arr[], int si, int ei) {
+  if (si >= ei)
+    return;
+
+  int mid = (si + ei) / 2;
+
+  mergeSort(arr, si, mid);     // left subarray
+  mergeSort(arr, mid + 1, ei); // right subarray
+
+  merge(arr, si, mid, ei); // conquer
+}
+
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  int arr[6] = {6, 3, 7, 5, 2, 4};
+  int n = 6;
+
+  cout<<"Array before:"<<endl;
+  printArray(arr, n);
+
+  mergeSort(arr, 0, n - 1);
+  cout<<"Array After:"<<endl;
+
+  printArray(arr, n);
+
+  return 0;
+}
