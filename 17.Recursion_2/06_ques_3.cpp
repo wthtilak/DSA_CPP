9a7f6f266afcca314c648cc7ea191361e2dcb4d7
Tilak <rjtilak36@gmail.com>
2026-04-22 16:44:07 +0530

    Practise Ques.

---
+++ 17.Recursion_2/06_ques_3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+// Question 3 : We are given a string S, we need to ﬁnd the count of all contiguous
+// substrings starting and ending with the same character. [Leetcode Premium Q]
+// Sample Input 1 : S = "abcab"
+// Sample Output 1 : 7
+
+// There are 15 substrings of "abcab" : a, ab, abc, abca, abcab, b, bc, bca, bcab, c, ca, cab, a, ab, b
+// Out of the above substrings, there are 7 substrings : a, abca, b, bcab, c, a and b. So, only
+// 7 contiguous substrings start and end with the same character.
+
+// Sample Input 2 : S = "aba"Sample
+// Output 2 : 4
+// The substrings are a, b, a and aba
+
+void contiguous_substr(string str, string ans[] ){
+
+    int n= str.size();
+}
+
+
+int main() {
+
+    string str = "abcab";
+    string ans[] = {};
+
+
+
+  return 0;
+}
