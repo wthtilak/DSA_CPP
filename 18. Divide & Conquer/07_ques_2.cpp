a60bfc336e9ae9e5a3d627991d806050828b90d5
Tilak <rjtilak36@gmail.com>
2026-04-26 11:08:35 +0530

    Majority Element (O(n logn))

--- 18. Divide & Conquer/07_ques_2.cpp
+++ 18. Divide & Conquer/07_ques_2.cpp
@@ -4,28 +4,15 @@
 using namespace std;
 
 // Given an array nums of size n, return the majority element. (MEDIUM)
-// The "majority element" is the element that appears more than ⌊n / 2⌋ times. You may assume
-// that the majority element always exists in the array.
-// Sample Input 1 : nums = [3,2,3]
-// Sample Output 1 : 3
-// Sample Input 2 : nums = [2,2,1,1,1,2,2]
-// Sample Output 2 : 2
+// The "majority element" is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array. Sample
+// Input 1 : nums = [3,2,3] Sample Output 1 : 3 Sample Input 2 : nums =
+// [2,2,1,1,1,2,2] Sample Output 2 : 2
 
-void printArray(const vector<int> &arr, int n) { // used const here for fast reading + cannot modify original.
-    int count =0;int element=0;
-    vector <int> temp[n];
-  for (auto i=0;i<n; i++) {
-    bool repeating = false;
-    for(int j=i+1;j<n; j++){
-      if(i == j){
-        if(!repeating){
-          temp->push_back(arr[i]);
-        }
+void printArray(vector<int> &arr, int n) { // Final Time Complexity : O(n logn)
+  // Total Space Complexity : O(n)
 
-        repeating = true;
-        break;
-      }
-    }
+  for (auto i : arr) {
     cout << i << " ";
   }
   cout << endl;
@@ -59,7 +46,7 @@ void merge(vector<int> &arr, int si, int mid, int ei) {
   }
 }
 
-void mergeSort(vector<int> &arr, int si, int ei) {// use & when passing vector int arrays for faster access.
+void mergeSort(vector<int> &arr, int si,int ei) { // use & when passing vector int arrays for faster access.
   if (si >= ei)
     return;
 
@@ -71,9 +58,10 @@ void mergeSort(vector<int> &arr, int si, int ei) {// use & when passing vector i
 }
 
 int main() {
-  vector<int> arr{2,2,1,1,1,2,2};
+  vector<int> arr{2, 2, 1, 1, 1, 2, 2};
 
   printArray(arr, arr.size());
-  // mergeSort(arr, 0, arr.size() - 1);
+  mergeSort(arr, 0, arr.size() - 1);
   printArray(arr, arr.size());
+  cout << "Majority Element = "<< arr[arr.size()/2]; // this is just array indexing so T.C & S.C = O(1).
 }
