842f143edb950b2771b63ddd0cbc7b3b324fbdf3
Tilak <rjtilak36@gmail.com>
2026-04-26 11:59:01 +0530

    Majority Element

--- 18. Divide & Conquer/07_ques_2.cpp
+++ 18. Divide & Conquer/07_ques_2.cpp
@@ -1,13 +1,14 @@
+// Given an array nums of size n, return the majority element. (MEDIUM)
+// The "majority element" is the element that appears more than ⌊n / 2⌋ times.
+// You may assume that the majority element always exists in the array. 
+
+// Sample Input 1 : nums = [3,2,3] Sample Output 1 : 3 Sample Input 2 : nums =
+// [2,2,1,1,1,2,2] Sample Output 2 : 2
 #include <iostream>
 #include <vector>
 #define endl '\n'
 using namespace std;
 
-// Given an array nums of size n, return the majority element. (MEDIUM)
-// The "majority element" is the element that appears more than ⌊n / 2⌋ times.
-// You may assume that the majority element always exists in the array. Sample
-// Input 1 : nums = [3,2,3] Sample Output 1 : 3 Sample Input 2 : nums =
-// [2,2,1,1,1,2,2] Sample Output 2 : 2
 
 void printArray(vector<int> &arr, int n) { // Final Time Complexity : O(n logn)
   // Total Space Complexity : O(n)
