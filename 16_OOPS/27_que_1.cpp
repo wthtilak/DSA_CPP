a885fcc214ac295486690c791bbff7a8c85f1827
Tilak <rjtilak36@gmail.com>
2026-04-27 23:48:32 +0530

    Fixed code indentations

--- 16_OOPS/27_que_1.cpp
+++ 16_OOPS/27_que_1.cpp
@@ -1,45 +1,41 @@
 // Create a class to store Complex numbers. Using operator overloading,
 // create the logic to subtract one complex number from another.
 
-// Note - In Complex numbers, the real part of 1st gets subtracted from the real part of 
-// 2nd number. Same goes for the imaginary part.
+// Note - In Complex numbers, the real part of 1st gets subtracted from the real
+// part of 2nd number. Same goes for the imaginary part.
 
 #include <iostream>
 #define endl '\n'
 using namespace std;
 
+class Complex {
+  int real, img;
 
-class Complex{
-    int real, img;
 public:
-    Complex(int real, int img){
-        this->real = real;
-        this->img = img;
-    }
-    void showNum(){
-        cout<<real << " + "<<img<<"i"<<endl;
-    }
+  Complex(int real, int img) {
+    this->real = real;
+    this->img = img;
+  }
+  void showNum() { cout << real << " + " << img << "i" << endl; }
 
-    //operator overloading
-    Complex operator -(Complex &C2){
-        int resReal = this->real - C2.real;
-        int resImg = this->img - C2.img;
+  // operator overloading
+  Complex operator-(Complex &C2) {
+    int resReal = this->real - C2.real;
+    int resImg = this->img - C2.img;
 
-        Complex c3 (resReal,resImg);
-        return c3;
-    }
+    Complex c3(resReal, resImg);
+    return c3;
+  }
 };
 
-int main()
-{
-    Complex c1(7,6);
-    Complex c2(2,5);
+int main() {
+  Complex c1(7, 6);
+  Complex c2(2, 5);
 
-    c1.showNum();
-    c2.showNum();
+  c1.showNum();
+  c2.showNum();
 
-    Complex c3 = c1-c2;
-    cout<<"res :";
-    c3.showNum();
-
+  Complex c3 = c1 - c2;
+  cout << "res :";
+  c3.showNum();
 }
